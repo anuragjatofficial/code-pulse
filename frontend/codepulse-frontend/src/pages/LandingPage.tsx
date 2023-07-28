10854d2c9490c9dc80bf46518d1f7acd1b57d8d9
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-28 19:11:58 +0530

    landing page

--- frontend/codepulse-frontend/src/pages/LandingPage.tsx
+++ frontend/codepulse-frontend/src/pages/LandingPage.tsx
@@ -3,11 +3,34 @@ import codepulseLogo from "../images/codepulseLogo.svg"
 
 export default function LandingPage() {
   return (
-    <div>
-         <img src={codepulseLogo} alt="" />
-        <div>
-           
-        </div>
+    <div style={{width:"100%"}}>
+         <img src={codepulseLogo} alt="logo" style={{width:"350px", margin:"auto",display:"block",marginTop:"30px", }}/>
+         <h1>Interview Master v-1.0.0</h1>
+
+<div>
+  <label>Type of Interview </label>
+  <select className="typeofinterview">
+    <option>Select a type</option>
+    <option>MERN</option>
+    <option>NODE</option>
+    <option>JAVA</option>
+  </select>
+</div>
+
+<div>
+  <br />
+  <label>Username : </label>
+  <input
+    type="text"
+    placeholder="Enter Username"
+    required
+    spellCheck="true"
+  />
+  <div></div>
+</div>
+
+<br />
+<button>Start</button>
     </div>
   )
 }

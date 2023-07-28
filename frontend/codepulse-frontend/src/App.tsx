61c17c7b8d9506f1c6a93c4ca0de73656ada02ff
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-28 18:41:39 +0530

    done

---
+++ frontend/codepulse-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

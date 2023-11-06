1b619c4f6ff32b7a79d237d73cb1aeb273a5902f
anuragjatofficial <anurag.1101140@gmail.com>
2023-11-06 23:42:59 +0530

    fixed `User.java` and changed table name from user -> users

--- backend/codePlus/src/main/java/com/codePlus/model/User.java
+++ backend/codePlus/src/main/java/com/codePlus/model/User.java
@@ -5,18 +5,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
